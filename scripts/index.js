53f63bba445c720a4249dda252aa1599a4f32bb9
davemflick <davemflick@yahoo.com>
2017-04-13 13:34:37 -0400

    mostly finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,18 @@
 
-var w = 800;
-var h= 600;
+const w = 800;
+const h= 400;
 
-var svg = d3.select(".mapCont").append("svg")
+const map = d3.select(".mapCont")
+
+const svg = map.append("svg")
    			.attr("width", w)
    			.attr("height", h);
 
-var projection = d3.geoMercator()
+const projection = d3.geoEquirectangular()
     .scale((w - 3) / (2 * Math.PI))
     .translate([w / 2, h / 2]);
 
-var path = d3.geoPath()
+const path = d3.geoPath()
     .projection(projection);
 
 
@@ -24,8 +26,10 @@ svg.append("use")
     .attr("xlink:href", "#sphere");
 
 svg.append("use")
-    .attr("class", "fill")
+    .attr("class", "water")
     .attr("xlink:href", "#globe");
+
+const gEl = svg.append("g");
 
 const reduceMass = d3.scaleLinear()
 				.domain([1000, 5000000])
@@ -38,18 +42,32 @@ const colorScale = d3.scaleThreshold()
 					.domain([1, 1000])
 					.range(colors)
 
+// Define div for tooltip
+const tooltip = map.append('div').attr("class", "tooltip");
+
+//Set zooming functionallity 
+const zoom = d3.zoom()
+		.scaleExtent([1,40])
+		.on('zoom', zooming)
+
+
+function zooming() {
+  gEl.attr("transform", d3.event.transform); 
+}
+
+svg.call(zoom)
 
 //Project the world with borders
 const worldURL = "https://unpkg.com/world-atlas@1/world/50m.json";
 d3.json(worldURL, function(error, world) {
   if(error){console.error(error)}
 
-  svg.insert("path")
+  gEl.insert("path")
       .datum(topojson.feature(world, world.objects.land))
       .attr("class", "land")
       .attr("d", path);
 
-  svg.insert("path", ".graticule")
+  gEl.insert("path", ".graticule")
       .datum(topojson.mesh(world, world.objects.countries, (x, y) => {return x !== y} ))
       .attr("class", "border")
       .attr("d", path);
@@ -59,12 +77,12 @@ d3.json(worldURL, function(error, world) {
 const URL = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json"
 d3.json(URL, (error, data)=>{
 	if(error){console.error(error)};
-	console.log(data)
 	
-	svg.selectAll("circle")
+	gEl.selectAll("circle")
 		.data(data.features)
 		.enter()
 		.append("circle")
+		.attr("class", "meteorites")
 		.style("z-index", 100)
 		.attr("cx", (d)=>{
 			return projection([Number(d.properties.reclong), Number(d.properties.reclat)])[0]
@@ -78,7 +96,29 @@ d3.json(URL, (error, data)=>{
 		.attr("fill", (d)=> {
 			return colorScale(d.properties.mass)
 		})
-		.style('opacity', .5);
+		.style('opacity', .5)
+		.on('mouseover', (d)=> tooltip.style("visibility", "visible")
+								.style("top", d3.event.pageY - 50 + 'px')
+								.style("left", d3.event.pageX + 'px')
+								.html(`Name: ${d.properties.name} 
+									<br> Mass: ${d.properties.mass}
+									<br> Fall: ${d.properties.fall}
+									<br> Class: ${d.properties.recclass}
+									<br> Latitude: ${d.properties.reclat}
+									<br> Longitude: ${d.properties.reclong}`))
+		.on('mouseout', (d)=> tooltipOut());
+
+	//Remove meteorite data on mouseout
+	const tooltipOut = (d) => {
+		tooltip.style("visibility", "hidden")
+	}
+
 	
 	})
 })
+
+
+
+
+
+
